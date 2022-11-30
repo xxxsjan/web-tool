1556cb47a6d8461c8320de339bc3313cbb7ed3f4
xxxsjan <626653354@qq.com>
2022-11-30 09:48:58 +0800

    添加路由 添加elementplus 添加css转对象

---
+++ src/layout.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <el-menu
+      :default-active="activeIndex"
+      class="el-menu-demo"
+      mode="horizontal"
+      router
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b"
+    >
+      <el-menu-item index="vscode-snippet-generator">vscode用户代码片段</el-menu-item>
+      <el-menu-item index="format-css-style">css style转对象</el-menu-item>
+    </el-menu>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+const activeIndex = ref('vscode-snippet-generator');
+</script>
+
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -7,4 +7,28 @@ import 'tailwindcss/tailwind.css';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 
-createApp(App).use(ElementPlus).mount('#app');
+import { createRouter, createWebHistory } from 'vue-router';
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'layout',
+      component: () => import('@/layout.vue'),
+      redirect: '/vscode-snippet-generator',
+      children: [
+        {
+          path: '/vscode-snippet-generator',
+          name: 'vscode-snippet-generator',
+          component: () => import('@/pages/vscode-snippet-generator.vue'),
+        },
+        {
+          path: '/format-css-style',
+          name: 'format-css-style',
+          component: () => import('@/pages/format-css-style.vue'),
+        },
+      ],
+    },
+  ],
+});
+createApp(App).use(router).use(ElementPlus).mount('#app');
