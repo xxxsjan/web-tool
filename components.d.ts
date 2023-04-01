624687f555406b71b22dd8dfce6d26c425445030
xxxsjan <626653354@qq.com>
2023-04-01 18:01:27 +0800

    perfect：使用hash路由

--- components.d.ts
+++ components.d.ts
@@ -17,12 +17,16 @@ declare module '@vue/runtime-core' {
     ElMenu: typeof import('element-plus/es')['ElMenu']
     ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
     ElOption: typeof import('element-plus/es')['ElOption']
+    ElRadio: typeof import('element-plus/es')['ElRadio']
+    ElRadioGroup: typeof import('element-plus/es')['ElRadioGroup']
     ElSelect: typeof import('element-plus/es')['ElSelect']
     IEpRefreshLeft: typeof import('~icons/ep/refresh-left')['default']
-    IEpShare: typeof import('~icons/ep/share')['default']
     LeftToRight: typeof import('./src/components/left-to-right.vue')['default']
     ResultDialog: typeof import('./src/components/result-dialog.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+  }
+  export interface ComponentCustomProperties {
+    vLoading: typeof import('element-plus/es')['ElLoadingDirective']
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
-import { createApp } from 'vue';
-import App from './App.vue';
+import { createApp } from "vue";
+import App from "./App.vue";
 
-import './style.css';
-import 'tailwindcss/tailwind.css';
+import "./style.css";
+import "tailwindcss/tailwind.css";
 
-import ElementPlus from 'element-plus';
-import 'element-plus/dist/index.css';
-import routes from './router/routes';
-import { createRouter, createWebHistory } from 'vue-router';
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import routes from "./router/routes";
+import {
+  createRouter,
+  createWebHistory,
+  createWebHashHistory,
+} from "vue-router";
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
-createApp(App).use(router).use(ElementPlus).mount('#app');
+createApp(App).use(router).use(ElementPlus).mount("#app");
