201a81eb6d7bb3ad3900f3907bc1da931eeef8f2
xxxsjan <626653354@qq.com>
2023-04-03 23:34:27 +0800

    图片裁剪优化自定义区域

--- public/worker.js
+++ public/worker.js
@@ -4,7 +4,7 @@ function getColor(x, y, imageData, width, height) {
     imageData.data[i],
     imageData.data[i + 1],
     imageData.data[i + 2],
-    imageData.data[i + 3],
+    imageData.data[i + 3]
   ];
 }
 function deltaE(lab1, lab2) {
@@ -14,19 +14,20 @@ function deltaE(lab1, lab2) {
   let deltaE = Math.sqrt(deltaL ** 2 + deltaA ** 2 + deltaB ** 2);
   return deltaE;
 }
-self.addEventListener("message", function (event) {
-  const { imageData, width, height } = event.data;
-  console.log("worker: ", imageData, width, height);
+self.addEventListener('message', function (event) {
+  const { imageData, width, height, start, end } = event.data;
+  console.log('worker: ', imageData, width, height);
   // 获取底部要截取像素
-  let bottom = height;
-  let top = -1;
+  let bottom = end || height;
+  let top = start - 1;
   let similarCount = 0;
   let precision = Math.floor(width * 0.1); // 一行 每几个像素检查一次
   let removeFlag = Math.floor((width / precision) * 0.6); //  每行抽样的点，有60%符合即可满足移除条件
   // 下边
-  for (let y = height - 1; y > height / 2; y--) {
+  let yEnd = end || height - 1;
+  for (let y = yEnd; y > height / 2; y--) {
     // 每行第一个点判断上一行结果
-    if (y !== height - 1) {
+    if (y !== yEnd) {
       // console.log("bottom", y + 1, similarCount, removeFlag);
       if (similarCount >= removeFlag) {
         bottom = y + 1;
@@ -43,20 +44,21 @@ self.addEventListener("message", function (event) {
       if (delta < 52) {
         similarCount++;
       } else {
-        console.log(
-          `%c ${y}行${x}像素：`,
-          `background-color:rgba(${result.toString()})`,
-          result,
-          delta
-        );
+        // console.log(
+        //   `%c ${y}行${x}像素：`,
+        //   `background-color:rgba(${result.toString()})`,
+        //   result,
+        //   delta
+        // );
       }
     }
   }
-  console.log("bottom: ", bottom);
+  console.log('bottom: ', bottom);
   // 上边
-  for (let y = 0; y < height; y++) {
+  let yStart = start || 0;
+  for (let y = yStart; y < height; y++) {
     // 一轮首次
-    if (y !== 0) {
+    if (y !== yStart) {
       // console.log("top", y - 1, similarCount, removeFlag);
       if (similarCount >= removeFlag) {
         top = y - 1;
@@ -69,7 +71,7 @@ self.addEventListener("message", function (event) {
     for (let x = 0; x < width; x = x + precision) {
       const result = getColor(x, y, imageData, width, height);
       const delta = deltaE(result, [0, 0, 0, 255]);
-      console.log("上边result: ", x, y, result, delta, similarCount);
+      // console.log("上边result: ", x, y, result, delta, similarCount);
 
       // 相比0, 0, 0, 255颜色接近
       if (delta < 30) {
