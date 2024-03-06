ada7d668212fe8a93b233f6f2d4c4ed5a0b91b52
xxxsjan <626653354@qq.com>
2024-03-06 15:25:57 +0800

    布局调整

--- src/pages/string-transform/string-transform.vue
+++ src/pages/string-transform/string-transform.vue
@@ -7,9 +7,17 @@
     <LessToCss id="LessToCss" />
     <EsmCommonjs id="EsmCommonjs" />
 
-    <el-card class="quick-nav">
-      <div class="quick-nav-item" v-for="item in list" :key="item">
-        <a :href="`#${item}`">{{ comToName(item) }}</a>
+    <el-card
+      class="quick-nav fixed top-2 overflow-auto max-w-max max-h-[500px]"
+    >
+      <div class="flex gap-1">
+        <div
+          class="quick-nav-item m-1 rounded h-7 px-2"
+          v-for="item in list"
+          :key="item"
+        >
+          <a :href="`#${item}`">{{ comToName(item) }}</a>
+        </div>
       </div>
     </el-card>
   </div>
@@ -37,22 +45,11 @@ function comToName(key: string) {
 .string-transform {
   scroll-behavior: smooth;
 }
-.quick-nav {
-  width: 200px;
-  max-height: 500px;
-  position: fixed;
-  right: 0;
-  top: 20%;
-  overflow: auto;
-}
 
 .quick-nav-item {
   display: flex;
   align-items: center;
-  justify-content: center;
-  height: 50px;
   background: #ecf5ff;
-  margin: 10px;
   color: #409eff;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import routes from './routes';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 
+NProgress.configure({
+  showSpinner: false // 可选配置，是否显示加载中的小圆圈
+});
 const router = createRouter({
   history: createWebHistory(),
   routes
+});
+router.beforeEach((to, from, next) => {
+  NProgress.start();
+  next();
+});
+
+router.afterEach(() => {
+  NProgress.done();
 });
 export default router;
