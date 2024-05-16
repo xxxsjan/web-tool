18eb01a80c86ac0838e5429fdc7da8b7581e205f
xxxsjan <626653354@qq.com>
2024-05-16 23:45:38 +0800

    feat: 修改 esm-to-commonjs.vue 组件，优化界面布局和功能

--- src/pages/string-transform/comps/esm-to-commonjs.vue
+++ src/pages/string-transform/comps/esm-to-commonjs.vue
@@ -3,14 +3,9 @@
     <div class="card bg-base-100 shadow-xl w-[700px]">
       <div class="card-body">
         <div class="card-title">
-          <label class="swap">
-            <input type="checkbox" v-model="mode" />
-            <div class="swap-on" @click="mode = 'esm'">esm转cjs(点击切换)</div>
-            <div class="swap-off" @click="mode = 'commonjs'">
-              cjs转esm(点击切换)
-            </div>
-          </label>
+          {{ mode === 'esm' ? 'esm转cjs' : 'cjs转esm' }}
         </div>
+
         <div class="flex flex-col justify-center items-center gap-4">
           <textarea
             v-model="code"
@@ -19,8 +14,12 @@
             resize="none"
             rows="10"
           ></textarea>
-
-          <button @click="run" class="btn btn-ghost">转换</button>
+          <div class="w-1/2 flex justify-between items-center">
+            <span @click="mode = mode === 'esm' ? 'commonjs' : 'esm'"
+              >切换</span
+            >
+            <button @click="run" class="btn btn-ghost">转换</button>
+          </div>
         </div>
       </div>
     </div>
@@ -33,25 +32,28 @@ defineExpose({
   title: 'esm导入 和 commonjs导入语法的相互切换',
 });
 const dialogVisible = ref(false);
+
 const esmText = `import path from "path"
 import Vue from 'vue';
 import {cloneDeep} from "lodash-es"
 import json from "./package.json"
 `;
-const code2DefaultText = `请在左侧输入代码`;
-const code = ref(esmText);
-const code2 = ref(code2DefaultText);
-const mode = ref('commonjs');
-
-watch(
-  () => mode.value,
-  value => {
-    if (value) {
-      code.value = `const path = require("path")
+const comText = `const path = require("path")
 const Vue = require('vue');
 const {cloneDeep} = require("lodash-es");
 const json = require("./package.json");
 `;
+const code2DefaultText = `请在左侧输入代码`;
+
+const code = ref(esmText);
+const code2 = ref(code2DefaultText);
+const mode = ref('esm');
+
+watch(
+  () => mode.value,
+  value => {
+    if (value !== 'esm') {
+      code.value = comText;
       code2.value = code2DefaultText;
     } else {
       code.value = esmText;
@@ -62,7 +64,7 @@ const json = require("./package.json");
 
 const run = () => {
   const code1Text = code.value.split('\n');
-  if (mode.value === 'commonjs') {
+  if (mode.value === 'esm') {
     code2.value = code1Text
       .map(item => {
         let reg = /import\s+(.*?)\s+from\s+["'`]([\w-\/.]+)["'`];?/;
