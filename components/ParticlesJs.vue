e19aaff067ebc3e28b9ff48b89ae58d9a75f2cf6
xiesongjian <xiesongjian@dataeye.com>
2025-03-21 12:45:26 +0800

    fix: 优化粒子效果速度

--- components/ParticlesJs.vue
+++ components/ParticlesJs.vue
@@ -68,7 +68,7 @@ onMounted(() => {
             },
             move: {
                 enable: true,
-                speed: 6,
+                speed: 3,
                 direction: 'none',
                 random: false,
                 straight: false,
